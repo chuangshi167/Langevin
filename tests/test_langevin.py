77a2ccec118803f5c5597cffff2b88027ccd35aa
Zane <hzeng3@u.rochester.edu>
2018-10-01 23:09:05 -0400

    Reconstruct the test class

--- tests/test_langevin.py
+++ tests/test_langevin.py
@@ -3,42 +3,17 @@
 
 """Tests for `langevin` package."""
 
+import unittest
 import pytest
-
-from click.testing import CliRunner
-
-from langevin import langevin
-from langevin import cli
-@pytest.fixture
-def test_drag_force():
-	"""
-	unit test for testing the function drag_force
-	"""
-	dragForce = drag_force(2, 3)
-	assert dragForce == -6
-	dragForce2 = drag_force(2.5, 2.5)
-	assert dragFOrce2 == -2.5 * 2.5 
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+import langevin.langevin as langevin
 
 
-def test_content():
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'langevin.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+class Test(unittest.TestCase):	
+	def test_drag_force(self):
+		"""
+		unit test for testing the function drag_force
+		"""
+		dragForce = langevin.drag_force(2, 3)
+		self.assertEqual(dragForce, -6)
+if __name__ == '__main__':
+	unittest.main()
