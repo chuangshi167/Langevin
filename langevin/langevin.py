03e894193c20138a33c2b8f3633e2e15b9c20925
Zane <hzeng3@u.rochester.edu>
2018-10-02 00:25:58 -0400

    Wrote three functions and unit tests

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -7,11 +7,31 @@ import scipy.stats as ss
 import matplotlib as plt
 
 
-def drag_force(damping_coefficient, velocity):
+def drag_force(gamma, velocity):
 	""" 
-	This function calculate the drag force in Langevin Dynamics
+	This function calculates the drag force in Langevin Dynamics
 	"""
-	return -damping_coefficient * velocity
+	return -gamma * velocity
 
+
+def check_range(location):
+	"""
+	This function tests if the molecule is within the range
+	"""
+	
+	if location < 5 and location > -5:
+		return True
+	else:
+		return False
+
+
+def random_force(T, _lambda, k_B = 1, delta = 1):
+	"""
+	This function generates random random noise (random force), throught the function noise = 2k_B * T * lambda * delta.
+	THe noise is generated normally from the mean, which is 0
+	"""
+	variance = 2 * k_B * T * _lambda * delta
+	sigma = np.sqrt(variance)
+	return np.random.normal(0, sigma)
 
 
--- tests/test_langevin.py
+++ tests/test_langevin.py
@@ -6,6 +6,8 @@
 import unittest
 import pytest
 import langevin.langevin as langevin
+import random
+import numpy as np
 
 
 class Test(unittest.TestCase):	
@@ -15,5 +17,29 @@ class Test(unittest.TestCase):
 		"""
 		dragForce = langevin.drag_force(2, 3)
 		self.assertEqual(dragForce, -6)
+	def test_check_range(self):
+		"""
+		unit test for testing the function check_range
+		"""
+		self.assertTrue(langevin.check_range(random.random() * 5))
+		self.assertTrue(langevin.check_range(random.random() * (-5)))
+		self.assertFalse(langevin.check_range(10))
+		self.assertFalse(langevin.check_range(-10))
+	def test_random_force(self):
+		"""
+		unit test for testing the function random_force
+		"""
+		_random_force = []
+		T = 273.15
+		_lambda = 0.07
+		theoretical_value = 2 * T * _lambda
+		for i in range(10000):
+			_random_force.append(langevin.random_force(T, _lambda))
+		mean = np.mean(_random_force)
+		variance = np.var(_random_force)
+		self.assertTrue(mean < 0.2 or mean > -0.2)
+		self.assertTrue(variance > 0.98 * theoretical_value or variance < 1.02 * theoretical_value)
+			
+		
 if __name__ == '__main__':
 	unittest.main()
